16e7f1c98dbd45ed3193d8a1f50a7e7e9c1f2fff
MalekeHan <Malekehanson@gmail.com>
2023-04-01 04:05:10 -0700

    added middleware

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,7 @@ const cors = require('cors'); //middleware allows requests from different origin
 const dotenv = require('dotenv'); //loads environment variables from a .env file
 const openaiRoutes = require('./routes/openaiRoutes');
 const Note = require('./models/noteModel');
+const { auth } = require('express-openid-connect');
 
 
 dotenv.config();  //Load variables from .env file into process.env
@@ -16,6 +17,26 @@ const app = express(); //Initialize express application
 app.use(express.json()); //Allows server to parse JSON data in request bodies
 app.use(cors()); //Enable CORS for all routes
 app.use('/api/openai', openaiRoutes);
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.SECRET_KEY,
+  baseURL: process.env.BASE_URL,
+  clientID: process.env.CLIENT_ID,
+  issuerBaseURL: process.env.ISSUER_BASE_URL,
+};
+
+
+app.use(auth(config));
+
+const { requiresAuth } = require('express-openid-connect');
+
+// Route that requires authentication
+app.get('/profile', requiresAuth(), (req, res) => {
+  res.send(JSON.stringify(req.oidc.user));
+});
+
 
 //Connect to MongoDB through the connection string stored in process.env.MONGO_URI
 mongoose.connect(process.env.MONGO_URI, {
@@ -29,9 +50,9 @@ const db = mongoose.connection;
 // Set up an event listener for when an error occurs during the database connection
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
-// Define a simple route for the root path of the API
+// req.isAuthenticated is provided from the auth router
 app.get('/', (req, res) => {
-  res.send('Welcome to Shared Notes API');
+  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
 });
 
 // Start the server and listen for incoming requests on the specified port
